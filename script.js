4d140dc20b25faa5fd9b0e65f42bf1b54161b98d
gaurav7058 <146345401+gaurav7058@users.noreply.github.com>
2024-08-27 19:40:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-//your JS code here. If required.
+document.getElementById('simpleForm').addEventListener("submit",(e)=>{
+    e.preventDefault();
+    let name=document.getElementById("name").value.trim();
+    let age=parseInt(document.getElementById('age').value.trim());
+    if (name==="" && isNaN(age)) {
+        alert("inputs cannot be empty.");
+    }
+})
