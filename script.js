61d02ade09dcfd4a805ceb0087e9d89ec9501372
gaurav7058 <146345401+gaurav7058@users.noreply.github.com>
2024-08-31 17:09:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,22 @@ document.getElementById('simpleForm').addEventListener("submit",(e)=>{
     e.preventDefault();
     let name=document.getElementById("name").value.trim();
     let age=parseInt(document.getElementById('age').value.trim());
+	let dataInfo=document.getElementById("data-info");
     if (name==="" && isNaN(age)) {
         alert("inputs cannot be empty.");
     }
+
+	let promise=new Promise((resolved,rejected)=>{
+    if(age>=18){
+			resolved(`Welcome, ${name}. You can vote.`)
+		}
+		else{
+			rejected(`Oh sorry ${name}. You aren't old enough.`)
+		}
+})
+promise.then((message)=>{
+    dataInfo.innerHTML=message
+}).catch(Error=>{
+    dataInfo.innerHTML=Error
+})
 })
